e827fff878ca5a265087856863b96142d87902f2
the-widget <brennenawana108ny@gmail.com>
2016-07-14 21:11:58 -0400

    added axios, setup AJAX request action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = 94badd64d670682ddf96dfc942e3575a
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
 
+  return {
+    type: FETCH_WEATHER
+    payload: request
+  };
+}
 
 
 
