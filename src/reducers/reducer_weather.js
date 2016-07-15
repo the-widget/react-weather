736d7b8bcb4f23924abc621b69ea9dcf2f0c8db8
the-widget <brennenawana108ny@gmail.com>
2016-07-14 23:16:40 -0400

    set up basic WeatherList table

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,15 @@
-export default function(state = null, action) {  
+import { FETCH_WEATHER } from '../actions/index'
+
+export default function(state = [], action) {
+  switch (action.type) {
+  case FETCH_WEATHER:
+    // return state.concat([action.payload.data]);
+    return [ action.payload.data, ...state ];
+  }
   return state;
 }
+
+// if we used
+// `state.push(action.payload.data)`
+// We would get an error
+// NEVER manipulate state, you MUST return a new fresh state object. 
