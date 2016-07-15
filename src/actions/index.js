28629d8ba99754ca625e5b6c675080c46380791a
the-widget <brennenawana108ny@gmail.com>
2016-07-14 20:43:18 -0400

    SearchBar configured. Generated weather API key.

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+const API_KEY = 94badd64d670682ddf96dfc942e3575a
+
+// You can seach weather forecast for 5 days with data every 3 hours by city name. All weather data can be obtained in JSON, XML or HTML format.
+// API call:
+
+// api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+// Parameters:
+// q city name and country code divided by comma, use ISO 3166 country codes
+
+// Examples of API calls:
+// api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,20 +4,21 @@ export default class SearchBar extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = { term: ''};
-
     this.onInputChange = this.onInputChange.bind(this);
   }
 
   onInputChange(event){
-    console.log(event.target.value)
     this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event){
+    event.preventDefault();
   }
 
   render() {
     return(
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input 
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
